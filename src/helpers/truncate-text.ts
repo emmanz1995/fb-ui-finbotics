b8c6ea2526cc289ce89301e568c0536c1aa59353
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2026-02-07 23:47:53 +0000

    adding type

--- src/helpers/truncate-text.ts
+++ src/helpers/truncate-text.ts
@@ -1,4 +1,4 @@
-export const truncateText = (text: string, n: number) => {
+export const truncateText = (text: string, n: number): string => {
   if (text.length === 0) return '';
 
   return text.length > n ? text.substring(0, n - 1) + '...' : text;
